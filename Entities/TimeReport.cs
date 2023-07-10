757e82fa6d14be03f7d8d2389ea0c67a806703e9
RasmusThn <rasmus795@gmail.com>
2023-07-10 20:52:00 +0200

    Create timeReport added

--- Entities/TimeReport.cs
+++ Entities/TimeReport.cs
@@ -1,17 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Entities
 {
     public class TimeReport
     {
         public int Id { get; set; }
+
+        [JsonPropertyName("workplace_id")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "The WorkplaceId field must be an integer.")]
         public int WorkplaceId { get; set; }
+
+        [JsonPropertyName("date")]
         public DateTime Date { get; set; }
+
+        [JsonPropertyName("hours")]
+        [Display(Name = "Hours")]
+        [Range(-1, 24, ErrorMessage = "The Hours field must be in the range 0 - 24.")]
         public float Hours { get; set; }
+
+        [JsonPropertyName("info")]
+        [Display(Name = "Info")]
         public string? Info { get; set; }
     }
 }
--- Services/TimeReportService.cs
+++ Services/TimeReportService.cs
@@ -1,9 +1,11 @@
 ﻿using Entities;
-using Newtonsoft.Json;
 using System;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Numerics;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Services
@@ -13,10 +15,12 @@ namespace Services
         private string myNotSoSecretToken = "3082b4e707da7cf9e8ebe69ab6e8c14c";
         private string _baseAddress = "https://arbetsprov.trinax.se/api/v1/";
         private HttpClient _httpClient;
+        private readonly JsonSerializerOptions _options;
 
         public TimeReportService()
         {
             _httpClient = CreateHttpClient();
+            _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
         }
 
         public async Task<TimeReport> GetSingleTimeReport(int reportId)
@@ -29,13 +33,9 @@ namespace Services
                 // Check if the request was successful (status code 200)
                 if (response.IsSuccessStatusCode)
                 {
-                    // Read the response content as a string
                     string responseData = await response.Content.ReadAsStringAsync();
+                    TimeReport timeReport = JsonSerializer.Deserialize<TimeReport>(responseData,_options);
 
-                    // Deserialize the JSON response into an instance of TimeReport
-                    TimeReport timeReport = JsonConvert.DeserializeObject<TimeReport>(responseData);
-
-                    // Return the deserialized time report
                     return timeReport;
                 }
                 else
@@ -50,18 +50,43 @@ namespace Services
                 throw new Exception("An error occurred during the API request.", ex);
             }
         }
+        public async Task<string> CreateTimeReport(TimeReport timeReport)
+        {
+            try
+            {
+                var formattedTimeReport = new
+                {
+                    workplace_id = timeReport.WorkplaceId,
+                    date = timeReport.Date.ToString("yyyy-MM-dd"),
+                    hours = timeReport.Hours.ToString("0.00"),
+                    info = timeReport.Info
+                };
+
+                var reportContent = JsonSerializer.Serialize(formattedTimeReport);
+                var bodyContent = new StringContent(reportContent, Encoding.UTF8, "application/json");
+               
+                HttpResponseMessage response = await _httpClient.PostAsync("timereport", bodyContent);
+
+                if (response.IsSuccessStatusCode) 
+                {
+                    return response.StatusCode.ToString();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch(Exception ex) 
+            {
+                throw new Exception("An error occurred during the API request.", ex);
+            }
+        }
         private HttpClient CreateHttpClient()
         {
-            // Create a HttpClient instance
             HttpClient httpClient = new HttpClient();
 
-            // Set the base URL for all actions
             httpClient.BaseAddress = new Uri(_baseAddress);
-
-            // Set the authorization token in the request header
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", myNotSoSecretToken);
-
-            // Set the Accept header to indicate that you expect JSON response
             httpClient.DefaultRequestHeaders.Accept.Clear();
             httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
